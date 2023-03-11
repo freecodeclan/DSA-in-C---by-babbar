e57650f3b3850e291c6455587035b7ed961ae814
meharsh05 <me.harshmehra07@gmail.com>
2023-03-11 07:09:43 +0530

    update

--- patterns.cpp
+++ patterns.cpp
@@ -573,3 +573,35 @@ int main(){
     } 
     return 0;
 }
+
+/* Example 23 - Draw a pattern
+         
+         1
+       2 2
+     3 3 3
+   4 4 4 4     */
+
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int i = 1;
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    while(i<=n){
+        int space = n-i;
+        while(space){
+            cout<<" ";
+            space = space-1;
+        }
+        int j = 1;
+        while(j<=i){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 int main(){
     int i = 1;
-    int n = 4;
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
     while(i<=n){
-        int space = i-1;
+        int space = n-i;
         while(space){
             cout<<" ";
             space = space-1;
         }
         int j = 1;
-        while(j<=n-i+1){
+        while(j<=i){
             cout<<i;
             j++;
         }
         cout<<endl;
         i++;
-    } 
+    }
     return 0;
 }
