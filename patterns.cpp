c5365a8fa7a5ed30d3685cf5c38f993b2bf1e44c
meharsh05 <me.harshmehra07@gmail.com>
2023-03-11 13:06:12 +0530

    update

--- patterns.cpp
+++ patterns.cpp
@@ -605,3 +605,36 @@ int main(){
     }
     return 0;
 }
+
+/* Example 24 - Draw a pattern
+    
+        1 2 3 4
+          2 3 4
+            3 4
+              4     */
+
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int i = 1;
+    int n = 4;
+    
+
+    while(i<=n){
+        int space = i-1;
+        while(space){
+            cout<<" ";
+            space = space-1;
+        }
+        int j = 1;
+        while(j<=n-i+1){
+            cout<<i+j-1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
