fc1c59ac86a8373c1c86bf375a75026f28652018
meharsh05 <me.harshmehra07@gmail.com>
2023-03-18 10:55:24 +0530

    u

--- binary&decimal.cpp
+++ binary&decimal.cpp
@@ -24,3 +24,29 @@ int main(){
     
    
 }
+
+/* Example to convert any binary number into decimal number */
+
+#include<iostream>
+#include<math.h>
+using namespace std; 
+
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    int i = 0;
+    int ans = 0;
+
+    while(n != 0){
+        int digit = n % 10;
+        if(digit == 1){
+            ans = ans + pow(2, i);
+        }
+        n = n/10;
+        i++;
+    }
+    cout<<"Answer is "<< ans <<endl;
+   
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<math.h> //- Used to include maths formulas 
+#include<math.h>
 using namespace std; 
 
 int main(){
@@ -7,18 +7,17 @@ int main(){
     cout<<"Enter the value of n: ";
     cin>>n;
 
-    int ans = 0;
     int i = 0;
+    int ans = 0;
 
-    while(n!=0){
-        int bit = n & 1; // used to find last bit of any number
-
-        ans = (bit*pow(10, i)) + ans; // 
-
-        n = n >> 1; // right shift to n by 1 means dividing n by 2.
+    while(n != 0){
+        int digit = n % 10;
+        if(digit == 1){
+            ans = ans + pow(2, i);
+        }
+        n = n/10;
         i++;
     }
     cout<<"Answer is "<< ans <<endl;
-    
    
 }
