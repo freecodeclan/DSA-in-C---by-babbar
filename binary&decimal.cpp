556cccb8e7584ac2ccc998651c80a20d2ee9a6ce
meharsh05 <me.harshmehra07@gmail.com>
2023-03-18 11:00:56 +0530

    u

--- binary&decimal.cpp
+++ binary&decimal.cpp
@@ -40,7 +40,7 @@ int main(){
     int ans = 0;
 
     while(n != 0){
-        int digit = n % 10;
+        int digit = n % 10; //- use to break any number into digit 
         if(digit == 1){
             ans = ans + pow(2, i);
         }
