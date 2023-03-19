74314f3873ab93d301f94dc1cfc8b689481a6a7a
meharsh05 <me.harshmehra07@gmail.com>
2023-03-19 09:06:03 +0530

    s

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<math.h>
+#include<math.h> //- Used to include maths formulas 
 using namespace std; 
 
 int main(){
@@ -7,17 +7,18 @@ int main(){
     cout<<"Enter the value of n: ";
     cin>>n;
 
-    int i = 0;
     int ans = 0;
+    int i = 0;
 
-    while(n != 0){
-        int digit = n % 10;
-        if(digit == 1){
-            ans = ans + pow(2, i);
-        }
-        n = n/10;
+    while(n!=0){
+        int bit = n & 1; // used to find last bit of any number
+
+        ans = (bit*pow(10, i)) + ans; // 
+
+        n = n >> 1; // right shift to n by 1 means dividing n by 2.
         i++;
     }
     cout<<"Answer is "<< ans <<endl;
+    
    
 }
