4c6c797e5a37562b35924477599138294deaefe7
meharsh05 <me.harshmehra07@gmail.com>
2023-03-23 19:47:06 +0530

    u

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -31,7 +31,7 @@ int main(){
         case '%' : cout << (a%b) <<endl;
                    break;
 
-        default : "This is default case";
+        default : cout << "Please Enter a valid operation" << endl;
     }
     return 0;
 }
