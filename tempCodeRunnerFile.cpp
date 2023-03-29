6ff645faaef32a7265cfe2a3f53ead54c8b898f5
meharsh05 <me.harshmehra07@gmail.com>
2023-03-29 17:34:07 +0530

    u

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
 using namespace std; 
 
-int main(){
-    int arr[15] = {1};
-    int n = 15; 
-    cout<<"Printing an array"<<endl;
-
-    for(int i = 1; i<n; i++){
+//- Function Created for an array
+void printarr(int arr[], int size){
+    cout<<"Printing an array "<<endl;
+    for(int i = 0; i<size; i++){
         cout<<arr[i]<<" ";
     }
+}
+
+int main(){
+    int first[10] = {2, 7};
+    printarr(first, 10);
+    
+    int second[5] = {2, 4, 6, 8, 10};
+    printarr(second, 5);
+    
     return 0;
 }
