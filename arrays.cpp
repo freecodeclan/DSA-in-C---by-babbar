ca04281755b33af33ea9ef83e11b45e3b9eb3afd
meharsh05 <me.harshmehra07@gmail.com>
2023-03-29 17:33:55 +0530

    upd

--- arrays.cpp
+++ arrays.cpp
@@ -13,3 +13,25 @@ int main(){
     return 0;
 }
 
+/*Creating a function for an array */
+
+#include<iostream>
+using namespace std; 
+
+//- Function Created for an array
+void printarr(int arr[], int size){
+    cout<<"Printing an array "<<endl;
+    for(int i = 0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int first[10] = {2, 7}; 
+    printarr(first, 10);
+    
+    int second[5] = {2, 4, 6, 8, 10};
+    printarr(second, 5);
+    
+    return 0;
+}
