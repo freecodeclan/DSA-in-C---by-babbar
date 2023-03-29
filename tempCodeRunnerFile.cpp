391d9eeddd1dd6814ba8dfb722e250ff78705f66
meharsh05 <me.harshmehra07@gmail.com>
2023-03-29 17:16:13 +0530

    u

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,11 +2,11 @@
 using namespace std; 
 
 int main(){
-    int arr[15] = {2,7};
+    int arr[15] = {1};
     int n = 15; 
     cout<<"Printing an array"<<endl;
 
-    for(int i = 0; i<n; i++){
+    for(int i = 1; i<n; i++){
         cout<<arr[i]<<" ";
     }
     return 0;
