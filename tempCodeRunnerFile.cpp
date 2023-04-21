e413fe5e01fd4e72d284c82feac18f3551351f9e
meharsh05 <me.harshmehra07@gmail.com>
2023-04-21 19:30:03 +0530

    u

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,23 @@
 #include<iostream>
 using namespace std; 
-
-bool iseven(int a){
-    if(a&1){
-        return 0;
-    }
-    else{
+bool isPrime(int n){
+    for(int i = 2; i<n; i++){
+        if(n%i==0){
+            return 0;
+        }
         return 1;
     }
 }
-
 int main(){
-    int num;
-    cout << "Enter the value of a: ";
-    cin >> num;
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
 
-    if(iseven(num)){
-        cout << "Number is even "<< endl;
-    }
+    if(isPrime(n)){
+        
+        cout<<"Is a prime number. "<<endl;
+}
     else{
-        cout << "Number is odd " << endl;
-    }
-    return 0;
+        cout<<"Is not a prime number. "<<endl;
+    }        
 }
