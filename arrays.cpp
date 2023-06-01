766bda8fa4f48318fce68698a14aacb3cd439eee
Harsh <harshmehra53@gmail.com>
2023-06-01 11:48:43 +0530

    up

--- arrays.cpp
+++ arrays.cpp
@@ -303,4 +303,41 @@ int main(){
     printSum(arr, 3, 3);
 }
 
+/* Print largest row sum and the row number */
+#include<bits/stdc++.h>
+using namespace std;
+int largestRowSum(int arr[][3], int i, int j){
+    int maxi = INT_MIN;
+    int rowIndex = -1;
 
+    for(int i = 0; i < 3; i++){
+        int sum = 0;
+        for(int j = 0; j < 3; j++){
+            sum += arr[i][j];
+        }
+        if(sum > maxi){
+            maxi = sum;
+            rowIndex = i;
+        }
+    }
+    cout << "The maximum sum is " << maxi << endl;
+    return rowIndex;
+}
+int main(){
+    int arr[3][3];
+    cout << "Enter the elements of an array" << endl;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> arr[i][j];
+        }
+    }
+    cout << "Printing the 2D Array" << endl;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << arr[i][j] << " ";       
+            }
+            cout << endl;
+        }
+        int ansIndex = largestRowSum(arr, 3, 3);
+        cout << "Max row at index " << ansIndex << endl;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,31 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+int largestRowSum(int arr[][3], int i, int j){
+    int maxi = INT_MIN;
+    int rowIndex = -1;
 
-// To print row wise sum
-void printSum(int arr[][3], int i, int j){
-    cout << "printing the sum " << endl; 
     for(int i = 0; i < 3; i++){
         int sum = 0;
         for(int j = 0; j < 3; j++){
             sum += arr[i][j];
         }
-        cout << sum << " ";
-    }   
+        if(sum > maxi){
+            maxi = sum;
+            rowIndex = i;
+        }
+    }
+    cout << "The maximum sum is " << maxi << endl;
+    return rowIndex;
 }
 int main(){
     int arr[3][3];
-
     cout << "Enter the elements of an array" << endl;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             cin >> arr[i][j];
         }
     }
+    cout << "Printing the 2D Array" << endl;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            cout << arr[i][j] << " ";
+            cout << arr[i][j] << " ";       
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
-    printSum(arr, 3, 3);
+        int ansIndex = largestRowSum(arr, 3, 3);
+        cout << "Max row at index " << ansIndex << endl;
 }
